7ba9dae189d8292db78bc5632a31d3145a61e438
kpkmaduwantha9 <kpkmaduwantha99@gmail.com>
2024-12-11 15:16:38 +0530

    final2

--- group13firm/group13hkv/views.py
+++ group13firm/group13hkv/views.py
@@ -5,12 +5,12 @@ from django.utils.timezone import now
 
 def hkvhome(request):
     if request.method == 'POST':
-        # Extract form data
+        
         member_name = request.POST.get('member_name')
         member_cpm = request.POST.get('member_cpm')
         investment_amount = request.POST.get('investment')
 
-        # Validate and save data
+       
         if member_name and member_cpm and investment_amount:
             Investment.objects.create(
                 member_name=member_name,
@@ -18,7 +18,7 @@ def hkvhome(request):
                 investment=investment_amount,
                 investment_date=now()
             )
-            return redirect('hkvhome')  # Redirect to the home page after successful submission
+            return redirect('hkvhome') 
 
     return render(request, 'hkv/HKVhome.html', {})
 from django.shortcuts import render
